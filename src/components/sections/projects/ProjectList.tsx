f7f10838b84445932e61c893321714880768e241
georgesycabaccang <georgesycabaccang@gmail.com>
2024-04-27 13:57:38 +0800

    Initial commit for layout. - Installed MUI - Implement Fade and Slide APIs from MUI

--- src/components/sections/projects/ProjectList.tsx
+++ src/components/sections/projects/ProjectList.tsx
@@ -1,3 +1,27 @@
-export default function ProjectList() {
-    return <div>projects section</div>;
+import { Card, Fade, Slide } from "@mui/material";
+
+export default function ProjectList({ section }: { section: string }) {
+    const direction = section === "proj" ? "left" : "right";
+
+    return (
+        <Fade
+            in={section === "proj"}
+            style={{ position: "absolute" }}
+            timeout={{ enter: 500, exit: 500 }}
+        >
+            <div>
+                <Slide
+                    direction={direction}
+                    in={section === "proj"}
+                    mountOnEnter
+                    unmountOnExit
+                    timeout={{ enter: 700, exit: 300 }}
+                >
+                    <Card sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
+                        Projects List
+                    </Card>
+                </Slide>
+            </div>
+        </Fade>
+    );
 }
