47c0965ef5afe5d0fe1f0a211de0e6933774c486
George <91815033+georgecabaccang@users.noreply.github.com>
2024-04-27 13:59:00 +0800

    Feature/layout (#4)
    * Change palette colors
    
    * Initial commit for layout.
    - Installed MUI
    - Implement Fade and Slide APIs from MUI

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,3 +1,27 @@
-export default function About() {
-    return <div>About</div>;
+import { Card, Fade, Slide } from "@mui/material";
+
+export default function About({ section }: { section: string }) {
+    const direction = section === "about" ? "left" : "right";
+
+    return (
+        <Fade
+            in={section === "about"}
+            style={{ position: "absolute" }}
+            timeout={{ enter: 500, exit: 500 }}
+        >
+            <div>
+                <Slide
+                    direction={direction}
+                    in={section === "about"}
+                    mountOnEnter
+                    unmountOnExit
+                    timeout={{ enter: 700, exit: 300 }}
+                >
+                    <Card sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
+                        About
+                    </Card>
+                </Slide>
+            </div>
+        </Fade>
+    );
 }
