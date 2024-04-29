5ce00621a3ce100d9d9db4d25c208a7b9e474ad1
georgesycabaccang <georgesycabaccang@gmail.com>
2024-04-29 13:02:58 +0800

    - Replace contants with enums in separate file. - Remove Slide, Fade, and Card usage for layout in functional components.

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,27 +1,3 @@
-import { Card, Fade, Slide } from "@mui/material";
-
-export default function About({ section }: { section: string }) {
-    const direction = section === "about" ? "left" : "right";
-
-    return (
-        <Fade
-            in={section === "about"}
-            style={{ position: "absolute" }}
-            timeout={{ enter: 500, exit: 500 }}
-        >
-            <div>
-                <Slide
-                    direction={direction}
-                    in={section === "about"}
-                    mountOnEnter
-                    unmountOnExit
-                    timeout={{ enter: 700, exit: 300 }}
-                >
-                    <Card sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
-                        About
-                    </Card>
-                </Slide>
-            </div>
-        </Fade>
-    );
+export default function About() {
+    return <div>About</div>;
 }
