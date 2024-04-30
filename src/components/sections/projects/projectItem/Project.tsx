407ee31d38ee24b83b74f6b538ebabcc1c29d9d2
georgesycabaccang <georgesycabaccang@gmail.com>
2024-04-30 18:06:32 +0800

    - Update style for project list and project list scrolling. - Change background color of sections in layout.module.css. - Change cursor for sections to auto.

--- src/components/sections/projects/projectItem/Project.tsx
+++ src/components/sections/projects/projectItem/Project.tsx
@@ -1,14 +1,15 @@
-import ProjectType from "../../../../types/ProjectType";
-import ProjectCard from "../../../ui/projectCard/ProjectCard";
-
+import { ReactNode } from "react";
 import styles from "./projectStyles.module.css";
+import ProjectTitle from "./ProjectTitle";
+import ProjectDescription from "./ProjectDescription";
+import ProjectTechs from "./ProjectTechs";
+import ProjectImage from "./ProjectImage";
 
-export default function Project({ project }: { project: ProjectType }) {
-    return (
-        <div className={styles.project}>
-            <ProjectCard>
-                <ProjectCard.Title>{project.title}</ProjectCard.Title>
-            </ProjectCard>
-        </div>
-    );
+export default function Project({ children }: { children: ReactNode }) {
+    return <div className={styles.project}>{children}</div>;
 }
+
+Project.Title = ProjectTitle;
+Project.Description = ProjectDescription;
+Project.Techs = ProjectTechs;
+Project.Image = ProjectImage;
