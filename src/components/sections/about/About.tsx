f325a5e16e2d9685f7ccd3e862879698044dd875
George <91815033+georgecabaccang@users.noreply.github.com>
2024-05-02 19:15:32 +0800

    Refactor About section (#20)
    - Remove max-width for text_group.
    - Refactor About section and SkillList.

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -1,18 +1,33 @@
 import SectionCard from "../../ui/cards/section-container/SectionCard";
+import TextGroup from "../../ui/cards/text-group-container/TextGroup";
 import SkillList from "../skills/SkillList";
 
 export default function About() {
     return (
         <SectionCard>
-            <span className={``}>Hi, my name is</span>
-            <h1 className={``}>George Alfred Cabaccang.</h1>
-            <p className={`list_item_body`}>
-                During the pandemic, I found myself with a lot of time on my hands, so I decided to
-                learn how to build websites. Now, I'm really into it, and I'm enjoying the process
-                of creating and improving my skills. It's been a rewarding way to make the most of a
-                challenging situation. So, here we are. . .
-            </p>
-            <SkillList />
+            <TextGroup>
+                <TextGroup.Header>
+                    <span>Hi, my name is</span>
+                </TextGroup.Header>
+
+                <TextGroup.Body>
+                    <h1>George Alfred Cabaccang.</h1>
+                </TextGroup.Body>
+
+                <TextGroup.Body>
+                    <p>
+                        During the pandemic, I found myself with a lot of time on my hands, so I
+                        decided to learn how to build websites. Now, I'm really into it, and I'm
+                        enjoying the process of creating and improving my skills. It's been a
+                        rewarding way to make the most of a challenging situation. So, here we are.
+                        . .
+                    </p>
+                </TextGroup.Body>
+
+                <TextGroup.Footer>
+                    <SkillList />
+                </TextGroup.Footer>
+            </TextGroup>
         </SectionCard>
     );
 }
--- src/components/sections/skills/SkillList.tsx
+++ src/components/sections/skills/SkillList.tsx
@@ -8,16 +8,21 @@ const SKILLS = ["JavaScript (ES6+)", "TypeScript", "React", "Node.js", "Java", "
 export default function SkillList() {
     return (
         <TextGroup>
-            <span className={`list_item_body`}>Technogolies I've been using recently:</span>
-            <ul className={styles.list}>
-                {SKILLS.map((skill, index) => {
-                    return (
-                        <li key={index} className={styles.list_item}>
-                            <Skill skill={skill} />
-                        </li>
-                    );
-                })}
-            </ul>
+            <TextGroup.Header>
+                <span>Technogolies I've been using recently:</span>
+            </TextGroup.Header>
+
+            <TextGroup.Body>
+                <ul className={styles.list}>
+                    {SKILLS.map((skill, index) => {
+                        return (
+                            <li key={index} className={styles.list_item}>
+                                <Skill skill={skill} />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </TextGroup.Body>
         </TextGroup>
     );
 }
