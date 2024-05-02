38deec3e7e477d7480baec452389aff2ca2cf128
George <91815033+georgecabaccang@users.noreply.github.com>
2024-05-02 19:06:45 +0800

    Refactor Experience feature (#19)
    * Change palette colors
    
    * -Remove unused components.
    - Refactor Experience.tsx and ExperienceList.tsx (Took out List.Item from Experience.tsx)

--- src/components/sections/experience/experienceItem/experience-summary-group/experience-summary/ExperienceSummary.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function ExperienceSummary({ children }: { children: ReactNode }) {
-    return <div className={`list_item_body `}>{children}</div>;
-}
