76c1e2d63dfeae62c8f9a8e2862a087264d092bc
George <91815033+georgecabaccang@users.noreply.github.com>
2024-05-02 18:36:39 +0800

    Update Experience components and layout (#18)
    * Change palette colors
    
    * Remove ExperienceHeader and replace with TextGroup component.

--- src/components/sections/experience/experienceItem/Experience.tsx
+++ src/components/sections/experience/experienceItem/Experience.tsx
@@ -1,31 +1,19 @@
 import ExperienceType from "../../../../types/ExperienceType";
 import ListItemCard from "../../../ui/cards/list/list-item/ListItem";
+import TextGroup from "../../../ui/cards/text-group-container/TextGroup";
 import ExperienceDate from "./ExperienceDate";
-import ExperienceSummaryGroup from "./experience-summary-group/ExperienceSummaryGroup";
-import ExperienceHeader from "./experience-summary-group/experience-header/ExperienceHeader";
 
 export default function Experience({ experience }: { experience: ExperienceType }) {
     return (
         <ListItemCard>
             <ExperienceDate>{experience.date}</ExperienceDate>
 
-            {/* START of Grouped text component*/}
-            <ExperienceSummaryGroup>
-                {/* START of Header of group */}
-                <ExperienceSummaryGroup.Header>
-                    <ExperienceHeader.Title>{experience.title}</ExperienceHeader.Title>
-                    <ExperienceHeader.Divider>{"•"}</ExperienceHeader.Divider>
-                    <ExperienceHeader.Employer>{experience.employer}</ExperienceHeader.Employer>
-                </ExperienceSummaryGroup.Header>
-                {/* END of Header of group */}
-
-                {/* START of Summary of employement */}
-                <ExperienceSummaryGroup.Summary>
-                    {experience.summary}
-                </ExperienceSummaryGroup.Summary>
-                {/* END of Summary of employement */}
-            </ExperienceSummaryGroup>
-            {/* END of Grouped text component*/}
+            <TextGroup>
+                <TextGroup.Header>
+                    <div>{`${experience.title} • ${experience.employer}`}</div>
+                </TextGroup.Header>
+                <TextGroup.Body>{experience.summary}</TextGroup.Body>
+            </TextGroup>
         </ListItemCard>
     );
 }
