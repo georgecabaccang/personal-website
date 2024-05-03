9a83ec6e606ce63d0c117f71d6e2dbae42063fe7
George <91815033+georgecabaccang@users.noreply.github.com>
2024-05-03 18:56:51 +0800

    Create 1280px media queries  (#21)
    - Create section title compound component.
    - Create media queries for 1280px.

--- src/components/ui/cards/section-container/SectionCard.tsx
+++ src/components/ui/cards/section-container/SectionCard.tsx
@@ -1,7 +1,10 @@
 import { ReactNode } from "react";
 
 import styles from "./sectionCard.module.css";
+import SectionTitle from "./SectionTitle";
 
 export default function SectionCard({ children }: { children: ReactNode }) {
     return <div className={styles.section_container}>{children}</div>;
 }
+
+SectionCard.Title = SectionTitle;
