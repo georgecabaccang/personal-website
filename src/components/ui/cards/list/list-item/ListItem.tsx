5dd843d1dfa15d7966cea02dfd0be757b4364870
georgesycabaccang <georgesycabaccang@gmail.com>
2024-05-04 18:15:20 +0800

    - Create 1024px to 1199px media queries.

--- src/components/ui/cards/list/list-item/ListItem.tsx
+++ src/components/ui/cards/list/list-item/ListItem.tsx
@@ -3,5 +3,5 @@ import { ReactNode } from "react";
 import styles from "./listItemCard.module.css";
 
 export default function ListItem({ children }: { children: ReactNode }) {
-    return <div className={styles.list_item}>{children}</div>;
+    return <li className={styles.list_item}>{children}</li>;
 }
